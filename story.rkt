bdf77b903b6269d93f9cc46d36db3737b23288ad
Teo G Nistor <teo.g.nistor@gmail.com>
2020-04-18 12:47:17 +0100

    Stuff

--- story.rkt
+++ story.rkt
@@ -1,16 +1,7 @@
 #lang racket
 
-(struct game-state (inventory room-contents location missions))
-(struct room (description doors fixed-objects))
-
-(define (parse-input-new input) ; TODO
-  (match input
-    [ "exit" 'exit]
-    [(regexp #rx"^go(| to| through| via)(| the) (.+)$" (list _ _ _ what)) 'go]
-    [(regexp #rx"^take(| the) (.+)$" (list _ _ what)) 'take]
-    [_ 'huh]))
-    
-; (define (look room
+(struct game-state (inventory room-contents room-descriptions room-doors location missions))
+;(struct room (description doors fixed-objects))
 
 (define (inventory-remove inventory object)
   (and (set-member? inventory object)
@@ -23,20 +14,6 @@
   (let ([given-room-contents (dict-ref room-contents room '())])
     (and (set-member? given-room-contents object)
          (dict-set room-contents room (set-remove given-room-contents object)))))
-
-(define (cannot-take object) ; TODO tell why
-  (display (string-append "You cannot pick up the " object ".\n")))
-
-(define (take game object)
-  (let ([updated-room-contents (room-remove (game-state-room-contents game) (game-state-location game) object)])
-    (if updated-room-contents
-        (begin
-          (display (string-append "You pick up the " object ".\n"))
-          (let ([updated-inventory (set-add (game-state-inventory game) object)])
-            (struct-copy game-state game [inventory updated-inventory][room-contents updated-room-contents])))
-        (begin
-          (cannot-take object)
-          game))))
 
 (define (achieve-mission missions mission)
   (if (member mission missions)
@@ -51,25 +28,78 @@
         (display "/3]\n")
         (cons mission missions))))
 
+(define (cannot-take object) ; TODO tell why
+  (display (string-append "You cannot pick up the " object ".\n")))
+
+
+(define (look game)
+  (begin
+    (display (dict-ref (game-state-room-descriptions game) (game-state-location game)))
+    game))
+
+(define (go game door)
+  ; TODO
+  game)
+
+(define (take game object)
+  (let ([updated-room-contents (room-remove (game-state-room-contents game) (game-state-location game) object)])
+    (if updated-room-contents
+        (begin
+          (display (string-append "You pick up the " object ".\n"))
+          (let ([updated-inventory (set-add (game-state-inventory game) object)])
+            (struct-copy game-state game [inventory updated-inventory][room-contents updated-room-contents])))
+        (begin
+          (cannot-take object)
+          game))))
+
+(define (talk game person)
+  ; TODO
+  game)
+
+(define (give game person object)
+  ; TODO
+  game)
+
+
+(define (bad-input game)
+  (begin
+    (display "You don't quite know ow to do that.")
+    game))
 
 ;(game-state-inventory (take (game-state '() (hash "rec" (set "chair" "desk")) "rec" '()) "chair"))
 ;(game-state-inventory (take (game-state '() (hash "rec" (set "chair" "desk")) "rec" '()) "desk"))
 ;(game-state-inventory (take (game-state '() (hash "rec" (set "chair" "desk")) "rec" '()) "hat"))
 ;(game-state-inventory (take (game-state '() (hash "rec" (set "chair" "desk")) "else" '()) "chair"))
-         
-         
-         
-         
 
-;;;; Old
 
-;(define (check-exit)
-;  (begin
-;    (display "Really exit? (y/n) ")
-;    (let ([input (read-line)])
-;      (if (or (not (string? input)) (string=? "y" (string-downcase input)))
-;          (void)
-;          (process-input)))))
+(define (check-exit)
+  (begin
+    (display "Really exit? (y/n) ")
+    (let ([input (read-line)])
+      (or (not (string? input)) (string=? "y" (string-downcase input))))))
+
+(define (parse-input input) ; TODO
+  (match input
+    [ "exit" #f]
+    [ "look" look]
+    [(regexp #rx"^go( to| through| via|)( the)? (.+)$" (list _ _ _ where)) (λ (game) (go game where))]
+    [(regexp #rx"^(take|pick up)( the)? (.+)$" (list _ _ _ what))          (λ (game) (take game what))]
+    [(regexp #rx"^talk( to the| to|) (.+)$" (list _ _ who))                (λ (game) (talk game who))]
+    [(regexp #rx"^give( the)? (.+) to( the)? (.+)$" (list _ _ what _ who)) (λ (game) (give game who what))]
+    [(regexp #rx"^give( the)? (.+)( the)? (.+)$" (list _ _ who _ what))    (λ (game) (give game who what))]
+    [_ bad-input]))
+
+(define (main-loop game)
+  (let* ([input (read-line)]
+         [action (and (string? input) (parse-input input))])
+    (if action
+        (main-loop (action game))
+        (if (check-exit)
+            game
+            (main-loop game)))))
+
+
+(main-loop '())
 
 ;(define (process-input)
 ;  (let ([input (read-line)])
